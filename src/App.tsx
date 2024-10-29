1ad29eabbff361e65061407d15917cbd8c2613eb
Yeongjun Joo <wndudwns6824@naver.com>
2024-10-30 00:43:49 +0900

    design : 퀴즈 생성 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import QuizPage from "./pages/QuizPage";
 import "./index.css";
+import CreateQuiz from "./pages/CreateQuiz";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/createquiz" element={<CreateQuiz />} />
       </Routes>
     </BrowserRouter>
   );
