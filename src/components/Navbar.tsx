3898fb6099a4649135cc5ac44c2a8c47c49691ff
Yeongjun Joo <wndudwns6824@naver.com>
2024-10-29 23:50:36 +0900

    design : 네브바 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 export default function Navbar({ scrollToBottom }: { scrollToBottom: () => void }) {
   return (
     <div className="flex items-center justify-between w-screen h-20 px-16">
-      <img src={Logo} className="w-[100px] h-30" />
+      <Link to="/">
+        <img src={Logo} className="w-[100px] h-30" />
+      </Link>
       <div className="flex items-center gap-10 text-xl font-semibold text-white">
-        <button className="hover:scale-105" onClick={scrollToBottom}>
-          시작하기
-        </button>
+        {window.location.pathname === "/" && (
+          <button className="hover:scale-105" onClick={scrollToBottom}>
+            시작하기
+          </button>
+        )}
         <button className="hover:scale-105">로그인</button>
         <button className="hover:scale-105">회원가입</button>
       </div>
