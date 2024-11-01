2c4ba4ffd07dac710dcc1171a8810f1a06b91f08
Yeongjun Joo <wndudwns6824@naver.com>
2024-11-01 20:25:02 +0900

    feat : 모달 기능 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 export default function Navbar({ scrollToBottom }: { scrollToBottom: () => void }) {
+  const aaaa = () => {
+    console.log(document.cookie);
+  };
   return (
     <div className="flex items-center justify-between w-screen h-20 px-16">
       <Link to="/">
@@ -13,7 +16,9 @@ export default function Navbar({ scrollToBottom }: { scrollToBottom: () => void 
             시작하기
           </button>
         )}
-        <button className="hover:scale-105">로그인</button>
+        <button className="hover:scale-105" onClick={aaaa}>
+          로그인
+        </button>
         <button className="hover:scale-105">회원가입</button>
       </div>
     </div>
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,33 +1,109 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Main from "../assets/bg.png";
 import Navbar from "../components/Navbar";
-import "aos/dist/aos.css";
 import QuizName from "../components/QuizName";
 import QuizModal from "../components/QuizModal";
 
+interface Quiz {
+  postId: number;
+  title: string;
+  content: string;
+  nickname: string;
+  profileImg: string;
+  memberId: number;
+  sex: number;
+}
+
+// let qq: Quiz[] = [
+//   {
+//     postId: 1,
+//     title: "월별 부정적 뉴스 수를 그래프",
+//     content: "지의 기간에서 최대 300개의 뉴스 제목을 가져와",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 1,
+//     sex: 0,
+//   },
+//   {
+//     postId: 2,
+//     title: "Google 감정 분석 API",
+//     content: "뉴스 API를 올바르게 연결한 후에",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 2,
+//     sex: 1,
+//   },
+//   {
+//     postId: 3,
+//     title: "111111111",
+//     content: "22222222222",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 1,
+//     sex: 0,
+//   },
+//   {
+//     postId: 4,
+//     title: "111111111",
+//     content: "22222222222",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 1,
+//     sex: 1,
+//   },
+//   {
+//     postId: 5,
+//     title: "111111111",
+//     content: "22222222222",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 1,
+//     sex: 0,
+//   },
+//   {
+//     postId: 6,
+//     title: "111111111",
+//     content: "22222222222",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 1,
+//     sex: 1,
+//   },
+//   {
+//     postId: 7,
+//     title: "111111111",
+//     content: "22222222222",
+//     nickname: "aass",
+//     profileImage:
+//       "https://blogthumb.pstatic.net/MjAyNDEwMzFfMjA1/MDAxNzMwMzU2ODYzNjMz.gZkji1Hc_Y8eaXd6St80zWPdpBA89ISjxZAu-bQ6q3Ag.3SOxAMvzPy-dPv8LkvIAy4OeMlq_IUWwbvUGJvRC6egg.JPEG/IMG_4926.JPG?type=s2",
+//     memberId: 1,
+//     sex: 0,
+//   },
+// ];
+
 export default function ListPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [quizList, setQuizList] = useState([]); // State for fetched quiz data
-  const bottomRef = useRef<HTMLDivElement>(null);
-
-  const scrollToBottom = () => {
-    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const handleQuizClick = () => {
+  const [quizList, setQuizList] = useState<Quiz[]>([]);
+  const [selectedQuiz, setSelectedQuiz] = useState<Quiz | null>(null);
+  const handleQuizClick = (quiz: Quiz) => {
     setIsModalOpen(true);
+    setSelectedQuiz(quiz);
   };
-
   const handleCloseModal = () => {
     setIsModalOpen(false);
+    setSelectedQuiz(null);
   };
-
-  // Fetch quiz data on component mount
   useEffect(() => {
     const fetchQuizList = async () => {
       try {
-        const response = await axios.get("/api/posts"); // Proxy handles '/api' prefix
+        const response = await axios.get("/api/posts");
         setQuizList(response.data);
         console.log(response.data);
       } catch (error) {
@@ -44,27 +120,38 @@ export default function ListPage() {
         style={{ backgroundImage: `url(${Main})` }}
       />
       <div className="relative">
-        <Navbar scrollToBottom={scrollToBottom} />
+        <Navbar />
       </div>
       <div className="flex flex-col items-center justify-start w-screen h-full">
-        <div className="flex flex-col items-center w-[70%] h-[80%] rounded-3xl shadow-xl backdrop-filter backdrop-blur bg-gradient-to-t from-[#7a7a7a1e] to-[#e0e0e024] bg-opacity-10 px-[1%] py-[1%]">
+        <div className="flex flex-col items-center w-[70%] min-h-[700px] h-auto rounded-3xl shadow-xl overflow-auto backdrop-filter backdrop-blur bg-gradient-to-t from-[#7a7a7a1e] to-[#e0e0e024] bg-opacity-10 px-[1%] py-[1%]">
           <div className="flex flex-col w-full h-[15%] items-center justify-center text-[25px]">
             게시물 목록
           </div>
-          <div className="grid grid-rows-3 grid-cols-3 justify-items-center items-center w-[95%] h-full">
+          <div className="grid grid-cols-3 justify-items-center items-center w-[95%] h-full">
             {quizList.map((quiz) => (
               <QuizName
                 key={quiz.postId}
-                postId={quiz.postId}
                 title={quiz.title}
+                sex={quiz.sex}
                 nickname={quiz.nickname}
-                onClick={handleQuizClick}
+                profileImg={quiz.profileImg}
+                onClick={() => handleQuizClick(quiz)}
               />
             ))}
           </div>
         </div>
       </div>
-      {isModalOpen && <QuizModal onClose={handleCloseModal} />}
+      {isModalOpen && selectedQuiz && (
+        <QuizModal
+          onClose={handleCloseModal}
+          memberId={selectedQuiz.memberId}
+          sex={selectedQuiz.sex}
+          title={selectedQuiz.title}
+          content={selectedQuiz.content}
+          profileImg={selectedQuiz.profileImg}
+          nickname={selectedQuiz.nickname}
+        />
+      )}
     </div>
   );
 }
